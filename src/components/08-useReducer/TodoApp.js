c489662bfcc27d6b3608c21ba82a83ef6849ce87
lgramirez <gramirez.developer@gmail.com>
2021-02-04 08:49:14 -0400

    Mark TODO as completed or pending

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -36,6 +36,13 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleToggle = (todoId) => {
+    dispatch({
+      type: "toggle",
+      payload: todoId,
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -67,7 +74,11 @@ export const TodoApp = () => {
             {todos.map((todo, index) => {
               return (
                 <li key={todo.id} className="list-group-item">
-                  <p className="text-center">
+                  <p
+                    // className={todo.done && "complete"}
+                    className={`${todo.done && "complete"}`}
+                    onClick={() => handleToggle(todo.id)}
+                  >
                     {index + 1}. {todo.desc}
                   </p>
                   <button
