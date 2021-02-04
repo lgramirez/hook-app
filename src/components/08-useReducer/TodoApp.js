60e4212778a0e65d0976a8ad42c9f2430b1f536a
lgramirez <gramirez.developer@gmail.com>
2021-02-04 09:19:00 -0400

    Optimization N1

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer } from "react";
 import { todoReducer } from "./todoReducer";
 import "./styles.css";
 import { useForm } from "../../hooks/useForm";
+import { TodoList } from "./TodoList";
 
 const initialState = [];
 
@@ -70,27 +71,11 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <ul className="list-group list-group-flush">
-            {todos.map((todo, index) => {
-              return (
-                <li key={todo.id} className="list-group-item">
-                  <p
-                    // className={todo.done && "complete"}
-                    className={`${todo.done && "complete"}`}
-                    onClick={() => handleToggle(todo.id)}
-                  >
-                    {index + 1}. {todo.desc}
-                  </p>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => handleDelete(todo.id)}
-                  >
-                    Borrar
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
+          <TodoList
+            todos={todos}
+            handleDelete={handleDelete}
+            handleToggle={handleToggle}
+          />
         </div>
         <div className="col-5">
           <h4>Agregar TODO</h4>
