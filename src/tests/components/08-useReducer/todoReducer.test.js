0e3ed2ef052b1829b21b14d3937bfe1e37b2cf5e
lgramirez <gramirez.developer@gmail.com>
2021-02-12 09:56:13 -0400

    More tests on reducer

--- src/tests/components/08-useReducer/todoReducer.test.js
+++ src/tests/components/08-useReducer/todoReducer.test.js
@@ -15,4 +15,21 @@ describe("Pruebas en todoReducer", () => {
     expect(state.find((item) => item.id === newTodo.id)).toEqual(newTodo);
     expect(state).toEqual([...demoTodos, newTodo]);
   });
+
+  test("debe de borrar un TODO", () => {
+    // action.payload = ID del TODO
+    const state = todoReducer(demoTodos, { type: "delete", payload: 1 });
+
+    expect(state.length).toBe(1);
+    expect(state.find((item) => item.id === 1)).toBe(undefined);
+    expect(state).toEqual([demoTodos[1]]);
+  });
+
+  test("debe de hacer el TOGGLE del TODO", () => {
+    // action.payload = ID del TODO
+    const state = todoReducer(demoTodos, { type: "toggle", payload: 1 });
+
+    expect(state[0].done).toBe(true);
+    expect(state[1]).toEqual(demoTodos[1]);
+  });
 });
