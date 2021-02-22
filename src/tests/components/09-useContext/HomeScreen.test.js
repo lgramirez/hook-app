d38bccf266d4ebb93cf36b2450b8a84db0ce386e
lgramirez <gramirez.developer@gmail.com>
2021-02-22 09:45:21 -0400

    Test with useContext

---
+++ src/tests/components/09-useContext/HomeScreen.test.js
@@ -0,0 +1,19 @@
+import { mount } from "enzyme";
+import { HomeScreen } from "../../../components/09-useContext/HomeScreen";
+import { UserContext } from "../../../components/09-useContext/UserContext";
+
+describe("Pruebas en <HomeScreen />", () => {
+  const user = {
+    name: "Gonzalo",
+    email: "gonzalo@gmail.com",
+  };
+
+  const wrapper = mount(
+    <UserContext.Provider value={{ user }}>
+      <HomeScreen />
+    </UserContext.Provider>
+  );
+  test("debe de mostrarse correctamente", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
